f2a9b0e29bce71971a7df367935e8de8bd7b8fff
spring-backend-mobdev <enieto@mobdev.cl>
2022-02-13 02:04:46 -0300

    Initial commit

---
+++ src/main/java/cl/mobdev/mockito/MockitoDocumentationApplication.java
@@ -0,0 +1,13 @@
+package cl.mobdev.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoDocumentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/mobdev/mockito/MockitoDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mobdev.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
